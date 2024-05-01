6ad714bfc74f5c558dc40ff1830ed1cb36304e40
gian <6262557@cegepmontpetit.ca>
2024-05-01 10:52:53 -0400

    FCT : TODO #08 Ajout de la classe Baladeur
    Définir la classe Baladeur sans compléter le code des méthodes

---
+++ R25TP05/BaladeurMultiFormats/Baladeur.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaladeurMultiFormats
+{
+    internal class Baladeur : IBaladeur
+    {
+        #region  Champs et Propriétés
+        /// <summary>
+        /// Initialise une instance de la classe Baladeur. Elle instancie la collection des chansons.
+        /// </summary>
+        private List<Chanson> m_colChansons;
+
+        /// <summary>
+        /// Nom du réportoire
+        /// </summary>
+        private const string NOM_RÉPERTOIRE = "Chansons";
+
+        public int NbChansons { get { return m_colChansons.Count; } }
+
+        #endregion
+        #region Méthodes
+        public void AfficherLesChansons(ListView pListView)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Chanson ChansonAt(int pIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConstruireLaListeDesChansons()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public void ConvertirVersAAC(int pIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConvertirVersMP3(int pIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConvertirVersWMA(int pIndex)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #region Constructeurs
+        /// <summary>
+        /// Initialise une instance de la classe Baladeur. Elle instancie la collection des chansons.
+        /// </summary>
+        public Baladeur() { m_colChansons = new List<Chanson>(); }
+        #endregion
+    }
+}
--- R25TP05/BaladeurMultiFormats/FrmPrincipal.cs
+++ R25TP05/BaladeurMultiFormats/FrmPrincipal.cs
@@ -26,6 +26,7 @@ namespace BaladeurMultiFormats
             ChansonAAC chansonAAC = new ChansonAAC("Chansons\\Blame it on me.aac") ;
             ChansonMP3 chansonMP3 = new ChansonMP3("Chansons\\Billie Jean.mp3");
             ChansonWMA chansonWMA = new ChansonWMA("Chansons\\Bad Guy.wma");
+            Baladeur baladeur = new Baladeur();
         }
         #endregion
         //---------------------------------------------------------------------------------
